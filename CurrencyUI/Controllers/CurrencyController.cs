86bf07cf195fe989669d1f807607af902a708987
CavidHuseinov <Cavid.huseynovv28@gmail.com>
2025-05-15 16:02:53 +0400

    add serilog. This serilog create all logs in folder

--- CurrencyUI/Controllers/CurrencyController.cs
+++ CurrencyUI/Controllers/CurrencyController.cs
@@ -10,18 +10,26 @@ namespace CurrencyUI.Controllers
     public class CurrencyController : Controller
     {
         private readonly ICurrencyService _services;
-
-        public CurrencyController(ICurrencyService services)
+        private readonly ILogger<CurrencyController> _logger;
+        public CurrencyController(ICurrencyService services, ILogger<CurrencyController> logger)
         {
             _services = services;
+            _logger = logger;
         }
 
         public async Task<IActionResult> Index()
         {
-            var json = await _services.GetCurrency();
-            var dto = JsonConvert.DeserializeObject<CurrencyVM>(json);
-            return View("~/Views/Currency/Index.cshtml", dto);
-
+            try
+            {
+                var json = await _services.GetCurrency();
+                var dto = JsonConvert.DeserializeObject<CurrencyVM>(json);
+                return View("~/Views/Currency/Index.cshtml", dto);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"Currency datalarin cekmek mumkun olmadi xeta:{ex.Message}");
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
--- CurrencyUI/Program.cs
+++ CurrencyUI/Program.cs
@@ -1,5 +1,6 @@
 using Currency.Business;
 using Currency.Core.Settings;
+using Serilog;
 
 namespace CurrencyUI
 {
@@ -13,6 +14,11 @@ namespace CurrencyUI
             builder.Services.AddHttpClient();
             builder.Services.AddMemoryCache();
             BusinessServiceRegistration.AddBusiness(builder.Services);
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            builder.Host.UseSerilog();
 
 
             var app = builder.Build();
