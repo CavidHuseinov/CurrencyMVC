61c51431d73782ecc9b83595718b49be3632f1f2
CavidHuseinov <Cavid.huseynovv28@gmail.com>
2025-05-15 16:12:01 +0400

    add background service work delay 24 hours and update currency datas

---
+++ Currency.Infrastructure/BackgroundServices/CurrencyBG.cs
@@ -0,0 +1,26 @@
+﻿
+using Currency.Business.Services.Interfaces;
+using Microsoft.Extensions.Hosting;
+
+namespace Currency.Infrastructure.BackgroundServices
+{
+    public class CurrencyBG : BackgroundService
+    {
+        private readonly ICurrencyService _currencyService;
+
+        public CurrencyBG(ICurrencyService currencyService)
+        {
+            _currencyService = currencyService;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await _currencyService.GetCurrency();
+
+                await Task.Delay(TimeSpan.FromHours(24), stoppingToken);
+            }
+        }
+    }
+}
---
+++ Currency.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,14 @@
+﻿
+using Currency.Infrastructure.BackgroundServices;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Currency.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static void AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddHostedService<CurrencyBG>();
+        }
+    }
+}
